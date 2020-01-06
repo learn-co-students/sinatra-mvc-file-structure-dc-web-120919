d72d3374051af7f4e0ee71eaad4b96b03dd405b0
github username <github email address>
2020-01-06 10:01:20 -0500

    Done.

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,9 +1,18 @@
-class Dog < Sinatra::Base
+class Dog 
+
+    @@all = []
+
+    attr_accessor :name, :breed, :age
 
     def initialize(name, breed, age)
         @name = name
         @breed = breed
         @age = age
+        @@all << self
+    end
+
+    def self.all
+        @@all
     end
 
 end
